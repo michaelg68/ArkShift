2778529175a85ef90a6fa234be3bdf24363cb692
Michael G <michael.goldgamer@gmail.com>
2016-04-10 20:57:09 +0300

    Revert to commit ff1efe4. Implement 2D arrays, rewrite Bricks.java - no extension of DynamicGameObject anymore

--- src/com/mmango/arkshift/Brick.java
+++ src/com/mmango/arkshift/Brick.java
@@ -4,14 +4,24 @@ import android.util.Log;
 
 import com.badlogic.androidgames.framework.DynamicGameObject;
 import com.badlogic.androidgames.framework.gl.TextureRegion;
+import com.badlogic.androidgames.framework.math.Rectangle;
+import com.badlogic.androidgames.framework.math.Vector2;
 
-public class Brick extends DynamicGameObject {
+
+	//public class Brick extends DynamicGameObject {
+	public class Brick {
+
 	public static final int BRICK_STATE_STILL = 0;
 	public static final int BRICK_STATE_MOVING_DOWN = 1;
 	public static final int BRICK_STATE_MOVING_UP = 2;
 
 	public static final float BRICK_WIDTH = 10.4f;
 	public static final float BRICK_HEIGHT = 10.4f;
+	
+	public final Vector2 position;
+	public final Rectangle bounds;
+	public final Vector2 velocity;
+	public final Vector2 accel;
 	
 /*	public static final int BRICK_COLOR_GOLD = 0;
 	public static final int BRICK_COLOR_GREEN = 1;
@@ -27,14 +37,34 @@ public class Brick extends DynamicGameObject {
 	public TextureRegion brickTexture;
 	public int color;
     int state;
-    float stateTime;  
+    float stateTime;
+    float x;
+    float y;
+    public int row;
+    public int column;
+    
+    public Brick(int column, int row, TextureRegion brickTexture) {
+    	this.column = column;
+    	this.row = row;
+
+    	x = World.FRAME_WIDTH + BRICK_WIDTH / 2 + BRICK_WIDTH * (float)column;
+    	y = World.WORLD_HEIGHT - World.NOTIFICATION_AREA_HEIGHT - World.FRAME_WIDTH - BRICK_WIDTH / 2 - BRICK_WIDTH * (float)row;
+        //super(x, y, BRICK_WIDTH, BRICK_WIDTH);
+    	velocity = new Vector2();
+		accel = new Vector2();
+    	this.position = new Vector2(x, y);
+		this.bounds = new Rectangle(x - BRICK_WIDTH / 2, y - BRICK_WIDTH / 2, BRICK_WIDTH, BRICK_WIDTH);
+	    this.brickTexture = brickTexture;
+        state = BRICK_STATE_STILL;
+        stateTime = 0;
+    }
 	
-    public Brick(float x, float y, TextureRegion brickTexture) {
+   /* public Brick(float x, float y, TextureRegion brickTexture) {
         super(x, y, BRICK_WIDTH, BRICK_WIDTH);
         this.brickTexture = brickTexture;
         state = BRICK_STATE_STILL;
         stateTime = 0;
-    }
+    }*/
     
     public void update(float deltaTime) {
 		//velocity.add(0 * deltaTime, 0 * deltaTime);
@@ -54,20 +84,14 @@ public class Brick extends DynamicGameObject {
 		stateTime += deltaTime;
 	}
     
-    public void moveDown(int row, int line) {
+    public void moveDown() {
     	//state=Brick.BRICK_STATE_MOVING_DOWN;
     	Log.d("Brick:moveDown", "moving the brick down");
-    	position.x = World.FRAME_WIDTH + Brick.BRICK_WIDTH / 2 + Brick.BRICK_WIDTH * row;
-    	position.y = World.WORLD_HEIGHT - World.NOTIFICATION_AREA_HEIGHT - World.FRAME_WIDTH - Brick.BRICK_WIDTH / 2 - Brick.BRICK_WIDTH * line;
-		bounds.lowerLeft.set(position).sub(bounds.width / 2, bounds.height / 2);
     }
 
-    public void moveUp(int row, int line) {
+    public void moveUp() {
     	//state=Brick.BRICK_STATE_MOVING_UP;
     	Log.d("Brick:moveUp", "moving the brick up");
-    	position.x = World.FRAME_WIDTH + Brick.BRICK_WIDTH / 2 + Brick.BRICK_WIDTH * row;
-    	position.y = World.FRAME_WIDTH + Brick.BRICK_WIDTH / 2 + Brick.BRICK_WIDTH * line;
-		bounds.lowerLeft.set(position).sub(bounds.width / 2, bounds.height / 2);    	
     }
 
 
