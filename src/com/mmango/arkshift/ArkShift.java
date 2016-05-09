eacb56df48e60416b3c0dd686d10d00c21e25f37
Michael G <michael.goldgamer@gmail.com>
2016-05-09 20:31:45 +0300

    Now Back button works in all screens except GameScreen

--- src/com/mmango/arkshift/ArkShift.java
+++ src/com/mmango/arkshift/ArkShift.java
@@ -3,7 +3,6 @@ package com.mmango.arkshift;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-
 //import android.content.SharedPreferences;
 import android.util.Log;
 
@@ -11,25 +10,25 @@ import com.badlogic.androidgames.framework.Screen;
 import com.badlogic.androidgames.framework.impl.GLGame;
 
 public class ArkShift extends GLGame {
-//	public static final String GAME_PREFERENCES = "ArkShiftPrefs";
+	// public static final String GAME_PREFERENCES = "ArkShiftPrefs";
 
 	boolean firstTimeCreate = true;
-	
 
 	@Override
 	public Screen getStartScreen() {
 		return new MainMenuScreen(this);
 	}
 
-
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		super.onSurfaceCreated(gl, config);
-		if(firstTimeCreate) {
-			Log.d("ArkShift:onSurfaceCreated", "Running Settings.readPrefs(glGame)");
-			
-			//Settings.clearAllPreferences(this);  //total clearing of all preferences
-			
+		if (firstTimeCreate) {
+			Log.d("ArkShift:onSurfaceCreated",
+					"Running Settings.readPrefs(glGame)");
+
+			// Settings.clearAllPreferences(this); //total clearing of all
+			// preferences
+
 			Settings.readPrefs(this);
 			Assets.load(this);
 			firstTimeCreate = false;
@@ -38,20 +37,28 @@ public class ArkShift extends GLGame {
 		}
 	}
 
-
 	@Override
 	public void onPause() {
 		super.onPause();
 		Log.d("ArkShift:onPause", "Running Settings.savePrefs(glGame)");
 		Settings.savePrefs(this);
-		//Settings.save(getFileIO());
-//		if (Settings.soundEnabled)
-//			Assets.music.pause();
+		// Settings.save(getFileIO());
+		// if (Settings.soundEnabled)
+		// Assets.music.pause();
 	}
-	
+
 	@Override
 	public void onBackPressed() {
-		setScreen(new MainMenuScreen(this));
+		getCurrentScreen();
+		String className = getCurrentScreen().getClass().getName();
+		// className = currentScreen = com.mmango.arkshift.HighscoreScreen
+		int len = className.split("\\.").length;
+		//Log.d("ArkShift:onBackPressed", "len = " + len);
+		className = className.split("\\.")[len - 1];
+		//Log.d("ArkShift:onBackPressed", "currentScreen = " + className);
+		if (!className.equals("GameScreen")) {
+			setScreen(new MainMenuScreen(this));
+		}
 	}
 
 }
